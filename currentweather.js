866fe347688bb7f4f05aa3be7631d9fc4ee191cb
kofithebass <kofi@kofihayford.com>
2021-03-18 21:30:12 -0400

    lots of changes to current weather JS
    Missing an equal sign in line 10 causing error. Missing city in the arrow function of line 4. Changed "data" variable name to "weatherData." Fixed numerous typos and now it is returning results

--- currentweather.js
+++ currentweather.js
@@ -1,19 +1,21 @@
 const key = "BIeUy48ueRxIJSGJAxmUNi7Y8stJrK3Z"
 
 // Create a function to execute the API and search for the city
-const citySearch = async () => {
+const citySearch = async (city) => {
 
     // Creating a variable for the link to the search function 
-    const centalLink = "http://dataservice.accuweather.com/locations/v1/cities/search";
+    const centralLink = "http://dataservice.accuweather.com/locations/v1/cities/search";
 
-    // Addiing query parameter to the end of the URL
-    const query = `?apikey=${key}&q${city}`;
+    // Addiing Makequery parameter to the end of the URL
+    const makeQuery = `?apikey=${key}&q=${city}`;
 
-    const response = await fetch(base + query);
-    const data = await response.json();
+    const response = await fetch(centralLink + makeQuery);
+    const weatherData = await response.json();
 
-    console.log(data);
+    console.log(weatherData);
 
 };
 //Call the City Search Function
-citySearch('NewYork')
+citySearch('London')
+
+
