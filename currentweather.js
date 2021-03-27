5d0a86248efc4f276e3177fdcffea671f72067fc
kofithebass <kofi@kofihayford.com>
2021-03-27 12:25:36 -0400

    Continuing to build function and connect the API

--- currentweather.js
+++ currentweather.js
@@ -1,5 +1,16 @@
 const key = "BIeUy48ueRxIJSGJAxmUNi7Y8stJrK3Z"
 
+//Bringing in the current conditions URL 
+const currentWeather = async (keyid) => {
+    const weatherLink = "http://dataservice.accuweather.com/currentconditions/v1/"
+    const makeQuery = `${keyid}?apikey=${key}`;
+
+    const response = await fetch(weatherLink + makeQuery);
+    const weatherData = await response.json();
+
+    return weatherData()
+};
+
 // Create a function to execute the API and search for the city
 const citySearch = async (city) => {
 
@@ -10,12 +21,20 @@ const citySearch = async (city) => {
     const makeQuery = `?apikey=${key}&q=${city}`;
 
     const response = await fetch(centralLink + makeQuery);
-    const weatherData = await response.json();
+    const cityData = await response.json();
 
-    console.log(weatherData);
+    return cityData[0];
 
 };
-//Call the City Search Function
+// Call the City Search Function
 citySearch('London')
+    .then(weatherData => {
+        //Returns a promise 
+        return currentWeather(weatherData.key);
+    }).then(weatherData => {
+        console.log(weatherData);
+        //Use Catch to capture any errors or mistakes
+    }).catch(err => console.log(err))
 
 
+currentWeather("328328")
