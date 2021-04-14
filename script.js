e04c27c8cdfcebb468451a05710e887969348ea1
kofithebass <kofi@kofihayford.com>
2021-04-13 21:13:42 -0400

    totally redid the javascript
    Broke it down into sections. First I had to make sure the search button was working and that data was getting pulled in from the API. Then had to make sure the right data was getting pulled. Once that happened, I realized that to get the windspeed and UV index for the forecast, I would have to use a different API call. Took a lot of trial and error to get that set up and working. Since they were firing off at the same time, I had to change use setTimeout to create a  slight delay to avoid the clash. After data was being pulled from the API, I than had to ensure that it was being connected to and displayed in the appropriate elements of the DOM. Once that was finally working, I had to then ensure it was logging to local storage. Used the inspector to check it along the way  to ensure it was logging.

--- script.js
+++ script.js
@@ -1,26 +1,118 @@
-//Createa a variable to store the user's entry into the search input box as an empty string. Will refer to it later
-let inputCity = "";
 
-//Declare the standard variabless
-const searchCity = $("#citySearch");
-const searchButton = $("#searchButton");
-const currentCity = $("#currentCity");
-const currentTemp = $("#temperature");
-const currentHumidity = $("#humidity");
-const currentWindSpeed = $("#windSpeed");
-const currentUvIndex = $("uvIndex");
-const clearHistoryBtn = $("#clearHistory");
-const cityName = [];
+$(document).ready(function () {
 
-//create a function to check the city name to ensure it matches an entry in the Accuweather database  
-function search(location) {
-    for (var i = 0; i < location.length; i++) {
-        if (location.toUpperCase() === location[i]) {
-            return -1;
-        }
+    //Declare a variable to store the searched city
+    let city = "";
+    let cityName;
+    // variable declaration
+    const kofisAPIkey = "1550e20e982ebfd203c3d9a82faf62a2"
+
+    let prevCities = localStorage.getItem('cities')
+    let searchCity = $("#search-city");
+    let searchButton = $("#search-button");
+    let clearButton = $("#clear-history");
+    let currentCity = $("#current-city");
+    let currentTemperature = $("#temperature");
+    let currentHumidty = $("#humidity");
+    let currentWSpeed = $("#wind-speed");
+    let currentUvindex = $("#uv-index");
+    let sCity = [...JSON.parse(prevCities)];
+    let fiveDayForecast = $("#fiveDayForecastAll");
+    let searchList = $(".list-group");
+
+    // searches the city to see if it exists in the entries from the storage. Create funnction for click button
+    searchButton.click(function (event) {
+        event.preventDefault();
+        let lat;
+        let long;
+        fiveDayForecast.empty()
+        console.log(searchCity.val(), 'this is jquery')
+        //set searchCity to city to instruct system to insert the value typed and submitted by search button into the empty city string
+        city = searchCity.val();
+        console.log(city)
+        // console.log(searchCity.val())
+        //use fetch to capture 
+        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${kofisAPIkey}`)
+            .then((res) => {
+                return res.json()
+            }).then((json) => {
+                lat = json[0].lat;
+                long = json[0].lon;
+                cityName = json[0].name;
+                captureSearch(cityName)
+                console.log(json)
+            }).catch((err) => {
+                console.log(err)
+            })
+        //used setTimeout because I got an error due to the fact that the API calls were firing of simulatneously. By delaying it, it now works fine. 
+        setTimeout(() => {
+            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=imperial&appid=${kofisAPIkey}`)
+                .then((res) => {
+                    //used console log to make sure that the data is being collected. Changed it to res.json to now actually send through to get data back. 
+                    return res.json()
+                }).then((json) => {
+                    insertWeatherData(json.current.temp, json.current.humidity, json.current.uvi, json.current.wind_speed)
+                    futureForecast(json.daily)
+                })
+                .catch((err) => {
+                    //used catch to figure out the error that was happening. 
+                    console.log(err)
+                })
+            //changed the lenght of the timeout gradually down to ensure good rersponsiveness but prevent API requests colliding 
+        }, 50);
+
+    })
+
+    function insertWeatherData(temp, hum, wind, uvi) {
+        currentTemperature.append(temp);
+        currentHumidty.append(hum)
+        currentWSpeed.append(wind)
+        currentUvindex.append(uvi)
+
     }
-    return 1;
-}
 
-//create a variable to store the API key 
-const key = "BIeUy48ueRxIJSGJAxmUNi7Y8stJrK3Z"
+    function futureForecast(daily) {
+        // console.log(daily, 'this is our daily')
+        //save time from having to call multiple times using map feature. 
+
+        daily.map((info, index) => {
+            if (index < 5) {
+                let currentDate = moment().add(index, 'days').calendar();
+                let temp = `<div class="col-sm-2 bg-primary forecast text-white ml-2 mb-3 p-2 mt-2 rounded" >
+            <p id="fDate0">${currentDate}</p>
+            <p id="fImg0"></p>
+            <p>Temp:<span id="fTemp0">${info.temp.day}</span></p>
+            <p>Humidity:<span id="fHumidity0">${info.humidity}</span></p>
+        </div>`
+                console.log(temp, 'this our temp')
+                fiveDayForecast.append(temp)
+            }
+        })
+    }
+
+    console.log(sCity.length)
+    function captureSearch(city) {
+        sCity.push(city);
+        localStorage.setItem(`cities`, JSON.stringify(sCity))
+    }
+    //USE if statement to capture the items that are coming through to local storage and have them display on the page in the right place. Save searches to local Storage and append them to a table.
+    if (sCity.length !== 0) {
+        sCity.map((index) => {
+            let temp = `
+        <li>
+        <div class="col-3 col-md m-1 bg-secondary">
+        <br>
+        <p>${index}</p>
+        <br>
+    </div>
+    </li>`
+            searchList.append(temp)
+        })
+    }
+    // These saved items should be clickable and should then pull up current conditions for the appropriate city.
+
+})
+
+//Clear History button should reset all results including flushing local storage. 
+
+
